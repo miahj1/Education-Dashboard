5c54e2cf1a582afc5dd689375427ceebb76fb03e
miahj1 <84815985+miahj1@users.noreply.github.com>
2023-02-01 11:28:20 -0500

    Hbar data added.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-JSC.Chart('chartDiv', {});
+JSC.Chart('chartDiv', {
+    type: 'horizontal column',
+    series: [
+       {
+          points: [
+             {x: 'Business', y: 1868711},
+             {x: 'Design', y: 1063148},
+             {x: 'Music', y: 846689},
+             {x: 'Web Development', y: 7981935}
+          ]
+       }
+    ]
+ });
+ 
